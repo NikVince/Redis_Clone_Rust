31e797926b7fa742985f5f242bb2193b9afde120
Nikolas (Daniel) Vincenti <165183152+NikVince@users.noreply.github.com>
2025-04-22 22:17:00 +0200

    UPDATED main.rs to bind listener to address in main function.

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,12 @@ use mini_redis::{Connection, Frame};
 
 #[tokio::main]
 async fn main() {
+    // Binding listener to address
+    let listener = Tcplistener::bind("127.0.0.1:6379").await.unwrap();
 
+    loop {
+        // The second item contains the IP and port of the new connection.
+        let (socket, _) = listener.accept().await.unwrap();
+        process(socket).await;
+    }
 }
