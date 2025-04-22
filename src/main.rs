8ca079eb21bdcdd8766e1e48e960accdefea6c74
Nikolas (Daniel) Vincenti <165183152+NikVince@users.noreply.github.com>
2025-04-22 22:25:29 +0200

    UPDATED main.rs with async process function.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
+use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
 async fn main() {
@@ -10,5 +10,19 @@ async fn main() {
         // The second item contains the IP and port of the new connection.
         let (socket, _) = listener.accept().await.unwrap();
         process(socket).await;
+    }
+}
+
+async fn process(socket: TcpStream) {
+    // The `Connection` lets us read/write redis **frames** instead of
+    // byte streams. The `Connection` type is defined by mini-redis.
+    let mut connection = Connection::new(socket);
+
+    if let Some(frame) = connection.read_frame().await.unwrap() {
+        println!("GOT: {:?}", frame);
+
+        // Respond with an error
+        let response = Frame::Error("unimplemented".to_string());
+        connection.write_frame(&response).await.unwrap();
     }
 }
